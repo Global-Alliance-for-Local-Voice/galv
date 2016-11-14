c57ae1741026e547d332f5d23351eb458ad4a9d4
Maija Barnett <maija@Maijas-MBP.home>
2016-11-14 10:41:02 -0500

    Fixed a bug in schedule.module, got rid of all warnings that Kellen had found, fixed the uid showing up in the schedue page that the inviter sees, did a code refactor in schedule.module and have some changed for private message in schedule.admin.inc. (These changes aren't done yet.)

--- sites/all/modules/custom/schedule/schedule_template.tpl.php
+++ sites/all/modules/custom/schedule/schedule_template.tpl.php
@@ -1,4 +1,4 @@
 <div>You have invited the following members to your video conference:</div>
-<?php foreach ($mypeople as $response => $name) : ?>
-  <?php print '<ul><li>' . $name . ' ' .  $response . '</li></ul>'; ?>
+<?php foreach ($mypeople as $uid => $message) : ?>
+  <?php print '<ul><li>' . $message . '</li></ul>'; ?>
 <?php endforeach; ?>
